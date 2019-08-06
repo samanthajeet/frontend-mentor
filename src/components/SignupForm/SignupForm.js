39266e915e7de0cc63f3715851c12ff4890b01dd
Samantha Jeet <samanthajeet@gmail.com>
2019-08-06 12:12:50 -0600

    initial view

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -7,17 +7,30 @@ class SignUpForm extends Component {
     return (
       <main>
         <section className="left">
-          <h1>
-            Learn to code by watching others
-          </h1>
-          <h5>See how experienced developers solve
-          problems in real-time. Watching scripted tutorials is great, but
-          understanding how developers think is invaluable.</h5>
+          <h1>Learn to code by watching others</h1>
+          <h5>
+            See how experienced developers solve problems in real-time. Watching
+            scripted tutorials is great, but understanding how developers think
+            is invaluable.
+          </h5>
         </section>
         <section className="right">
-          Try it free 7 days then $20/mo. thereafter First Name Last Name Email
-          Address Password Claim your free trial By clicking the button, you are
-          agreeing to our Terms and Services
+          <section className="right-header rounded-corners">
+            <h5>
+              <span className="bold">Try it free 7 days</span> then $20/mo.
+              thereafter{" "}
+            </h5>
+          </section>
+          <section className="inputs rounded-corners">
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+            <input type="text" placeholder="Email Address" />
+            <input type="password" placeholder="Password" />
+            <button>Claim your free trial</button>
+            <section className="footer">
+              By clicking the button, you are agreeing to our <span className="redtext">Terms and Services</span> 
+            </section>
+          </section>
           <footer>
             <p class="attribution">
               Challenge by{" "}
