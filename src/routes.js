941872f3342e63d242fe56b2f5e67dc7553fad15
Samantha Jeet <samanthajeet@gmail.com>
2019-08-07 13:24:39 -0600

    hacky sack

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import App from './App'
 import SignupForm from './components/SignupForm/SignupForm';
+import DarkTheme from './components/LandingDarkTheme/LandingDark'
 
 export default (
   <Switch>
     <Route path='/' exact component={App} />
     <Route path='/signupform' component={SignupForm} />
+    <Route path='/darktheme' component={DarkTheme} />
   </Switch>
 )
